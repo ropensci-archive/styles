5e7ca6b9e24868200be5896e007d58ceb3b47f30
lizhmartin <emartin@student.unimelb.edu.au>
2017-10-27 12:07:19 +1100

    add one example internal style, better

---
+++ R/better.R
@@ -0,0 +1,32 @@
+#' @title Better
+#'
+#' @description Internal style
+#'
+better <- new_style(par = list(pch = 16,
+                             fg = grey(0.6),
+                             col.axis = grey(0.4),
+                             col.lab = grey(0.2),
+                             las = 1,
+                             col.main = grey(0.4),
+                             col.sub = grey(0.4),
+                             tcl = -0.25,
+                             bty="l"),
+                  graphics = list(axis = list(hadj = 0.5),
+                                  hist.default = list(col = "navyblue",
+                                                      border = "white",
+                                                      main = "",
+                                                      ylab = "count"),
+                                  plot.xy = list(col = colour,
+                                                 lwd = 2.5,
+                                                 cex = 1.1),
+                                  barplot.default = list(col = "navyblue",
+                                                         border = NA,
+                                                         space = 0.3),
+                                  boxplot.default = list(col = scales::alpha("navyblue", 0.5),
+                                                         border = "navyblue",
+                                                         pars = list(boxwex = 0.8,
+                                                                     staplewex = 0.5,
+                                                                     outwex = 0.5,
+                                                                     cex = 1,
+                                                                     whisklty = "solid")),
+                                  title = list(line = 2)))
